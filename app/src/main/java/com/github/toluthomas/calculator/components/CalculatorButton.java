9d24f0507c9997d0d0a37cf4a3e8b16d0d91aef8
Tolu Thomas <tolu.thomas@protonmail.com>
2021-11-07 09:11:15 +0100

    Refactored to hide warnings

--- app/src/main/java/com/github/toluthomas/calculator/components/CalculatorButton.java
+++ app/src/main/java/com/github/toluthomas/calculator/components/CalculatorButton.java
@@ -7,41 +7,49 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.res.ResourcesCompat;
 
 @SuppressLint("ViewConstructor")
 public class CalculatorButton extends androidx.appcompat.widget.AppCompatButton {
     private Button button;
     final String defaultButtonName = "";
-    @SuppressLint("UseCompatLoadingForDrawables")
-    final Drawable defaultButtonBackground = getResources().getDrawable(android.R.drawable.btn_default);
+    final Drawable defaultButtonBackground = ResourcesCompat.getDrawable(getResources(), android.R.drawable.btn_default,
+            null);
     final int defaultIndex = 0;
     final int defaultWidth = 100;
     final int defaultHeight = 100;
     final ViewGroup.LayoutParams defaultLayoutParams = new ViewGroup.LayoutParams(defaultWidth, defaultHeight);
 
-    public CalculatorButton(@NonNull Context context, String buttonName, Drawable background, int index, ViewGroup.LayoutParams layoutParams) {
+    public CalculatorButton(@NonNull Context context, String buttonName, Drawable background, int index,
+            ViewGroup.LayoutParams layoutParams) {
         super(context);
         this.button = new Button(context); // Create new instance of button
         this.button.setText(buttonName == null ? defaultButtonName : buttonName); // Set button text
-        this.button.setBackground(background == null ? defaultButtonBackground : background); // Give button some background
+        this.button.setBackground(background == null ? defaultButtonBackground : background); // Give button some
+                                                                                              // background
         this.button.setId(index); // Set the ID for each button
-        this.button.setLayoutParams(layoutParams == null ? defaultLayoutParams : layoutParams); // Set width and height of button
+        this.button.setLayoutParams(layoutParams == null ? defaultLayoutParams : layoutParams); // Set width and height
+                                                                                                // of button
     }
 
     // If index is not supplied
-    public CalculatorButton(@NonNull Context context, String buttonName, Drawable background, ViewGroup.LayoutParams layoutParams) {
+    public CalculatorButton(@NonNull Context context, String buttonName, Drawable background,
+            ViewGroup.LayoutParams layoutParams) {
         super(context);
         this.button.setText(buttonName == null ? defaultButtonName : buttonName); // Set button text
-        this.button.setBackground(background == null ? defaultButtonBackground : background); // Give button some background
+        this.button.setBackground(background == null ? defaultButtonBackground : background); // Give button some
+                                                                                              // background
         this.button.setId(defaultIndex); // Use default index as ID
-        this.button.setLayoutParams(layoutParams == null ? defaultLayoutParams : layoutParams); // Set width and height of button
+        this.button.setLayoutParams(layoutParams == null ? defaultLayoutParams : layoutParams); // Set width and height
+                                                                                                // of button
     }
 
     // If no index or no layout params are specified
     public CalculatorButton(@NonNull Context context, String buttonName, Drawable background) {
         super(context);
         this.button.setText(buttonName == null ? defaultButtonName : buttonName); // Set button text
-        this.button.setBackground(background == null ? defaultButtonBackground : background); // Give button some background
+        this.button.setBackground(background == null ? defaultButtonBackground : background); // Give button some
+                                                                                              // background
         this.button.setId(defaultIndex); // Use default index as ID
         this.button.setLayoutParams(defaultLayoutParams); // Use default width and height
     }
@@ -56,7 +64,7 @@ public class CalculatorButton extends androidx.appcompat.widget.AppCompatButton 
     }
 
     // Return the generated button
-    public Button getButton(){
+    public Button getButton() {
         return this.button;
     }
 }
